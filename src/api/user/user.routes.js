f55b123c94798a68ca51e6bbbca45816a52e9043
NoelCordova <noelespinocordova@gmail.com>
2019-06-09 09:50:50 -0500

    WIP: api user

--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const { getUser, updateUser, deleteUser } = require('./user.controller');
+const { validateTokenExpiration } = require('../../middlewares/validators');
 
-router.post('', []);
+router.get('/:username', [validateTokenExpiration, getUser]);
+router.put('/:username', [validateTokenExpiration, updateUser]);
+router.delete('/:username', [validateTokenExpiration, deleteUser]);
 
 module.exports = router;
