aca87fd425b4f198e1db321a05f53ec2dbde1160
NoelCordova <noelespinocordova@gmail.com>
2019-08-02 17:20:52 -0500

    validators funcs

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -1,10 +1,7 @@
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
-const { handleError } = require('../services/utils');
 const Config = require('../models/config.model');
 const { ROLE_ADMIN, TOKEN_SECRET_KEY } = require('../config');
-
-const fileSrc = 'src/middlewares/validators.js';
 
 /**
  * Function that validates the credentials for signup
@@ -96,50 +93,42 @@ const validateCredentialsLogin = (req, res, next) => {
  * Function that validates the body for a edit user as admin
  */
 const validateUserInfoAsAdmin = (req, res, next) => {
-  const body = req.body;
-  const validationSchema = Joi.object().keys({
-    email: Joi.string()
-      .regex(/^[a-zA-Z0-9_.-]*@(gmail|hotmail)(.com)$/)
-      .required(),
-    username: Joi.string()
-      .regex(/^[a-zA-Z_]{8,16}/)
-      .required(),
-    password: Joi.string()
-      .regex(/^[a-zA-Z0-9_]{5,10}$/)
-      .required(),
-    role: Joi.string()
-      .equal([process.env.ROLE_ADMIN, process.env.ROLE_USER])
-      .required()
-  });
+  try {
+    const { body } = req;
+    const validationSchema = Joi.object().keys({
+      email: Joi.string()
+        .regex(/^[a-zA-Z0-9_.-]*@(gmail|hotmail)(.com)$/)
+        .required(),
+      username: Joi.string()
+        .regex(/^[a-zA-Z_]{8,16}/)
+        .required(),
+      password: Joi.string()
+        .regex(/^[a-zA-Z0-9_]{5,10}$/)
+        .required(),
+      role: Joi.string()
+        .equal([process.env.ROLE_ADMIN, process.env.ROLE_USER])
+        .required(),
+    });
 
-  const validResult = Joi.validate(
-    {
-      email: body.email,
-      username: body.username,
-      password: body.password,
-      role: body.role
-    },
-    validationSchema
-  );
+    const validResult = Joi.validate(
+      {
+        email: body.email,
+        username: body.username,
+        password: body.password,
+        role: body.role,
+      },
+      validationSchema,
+    );
 
-  if (validResult.error !== null) {
-    if (validResult.error.details[0].path[0] === 'role')
-      return handleError(
-        res,
-        400,
-        'El rol ingresado no es válido',
-        fileSrc,
-        56
-      );
-    else
-      return handleError(
-        res,
-        400,
-        validResult.error.details[0].message,
-        fileSrc,
-        64
-      );
-  } else next();
+    if (validResult.error !== null) {
+      res.status(400);
+      throw new Error(validResult.error.details[0].message);
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 /**
@@ -205,33 +194,33 @@ const validateTokenRole = (req, res, next) => {
  * Function that validates the identity (email or username) in the session token
  */
 const validateTokenIdentity = (req, res, next) => {
-  const Authorization = req.get('Authorization');
-  const email = req.params.email;
+  try {
+    const Authorization = req.get('Authorization');
+    const { email } = req.params;
 
-  if (Authorization === undefined)
-    return handleError(
-      res,
-      401,
-      'No tienes autorización para acceder a esta ruta',
-      fileSrc,
-      157
-    );
+    if (Authorization === undefined) {
+      res.status(401);
+      throw new Error('You dont have the privilieges to access this route [no token]');
+    }
 
-  const token = Authorization.split(' ')[1];
+    const token = Authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.TOKEN_SECRET_KEY, (error, payload) => {
-    if (error) return handleError(res, 401, error.name);
+    jwt.verify(token, TOKEN_SECRET_KEY, (error, payload) => {
+      if (error) {
+        res.status(401);
+        throw new Error('You dont have the privilieges to access this route [bad token]');
+      }
 
-    payload.email !== email
-      ? handleError(
-        res,
-        401,
-        'No tienes autorización para acceder a esta ruta: [email]',
-        fileSrc,
-        171
-      )
-      : next();
-  });
+      if (payload.email !== email) {
+        res.status(401);
+        throw new Error('You dont have the privilieges to access this route [email]');
+      } else {
+        next();
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
